51ed1f7e7b8c8164e1198655bd21a40e8cb0e5a0
datta <dattatraysalunkhe832@gmail.com>
2024-02-04 21:17:06 +0530

    changes in file

--- api/routes/booking.route.js
+++ api/routes/booking.route.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { verifyToken } from '../utils/verify.User.js'
 import { create, getbooking } from '../Controllers/Booking.controller.js'
 
+
 const router = express.Router()
 
 router.route('/create').post(verifyToken,create)
