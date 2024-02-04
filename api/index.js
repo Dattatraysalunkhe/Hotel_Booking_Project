c89520a981a575dd760c5602f2016fe73dcc609f
datta <dattatraysalunkhe832@gmail.com>
2024-02-04 20:16:31 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,15 +3,17 @@ import mongoose from 'mongoose'
 import { DB_NAME } from './constant.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-
+import path from 'path'
 
 dotenv.config();
 
 mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`).then(() => {
     console.log("Monogodb connected");
 }).catch((error) => {
-   console.log("Mongodb connection failed",error)
+    console.log("Mongodb connection failed", error)
 })
+
+const __dirname = path.resolve();
 
 const app = express()
 
@@ -23,35 +25,41 @@ app.use(cookieParser())
 
 
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`port isliting on ${port}`)
 })
 
 import userRouter from './routes/user.route.js'
 
-app.use("/api/user",userRouter)
+app.use("/api/user", userRouter)
 
 import authRouter from './routes/auth.route.js'
 
-app.use("/api/auth",authRouter)
+app.use("/api/auth", authRouter)
 
 import listingRouter from './routes/listing.route.js'
 
-app.use('/api/listing',listingRouter)
+app.use('/api/listing', listingRouter)
 
 import bookingrouter from './routes/booking.route.js'
 
-app.use('/api/booking',bookingrouter)
+app.use('/api/booking', bookingrouter)
 
-app.use((err,req,res,next) => {
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Servar Error';
     return res
-    .status(statusCode)
-    .json({
-        success:false,
-      message,
-      statusCode,
+        .status(statusCode)
+        .json({
+            success: false,
+            message,
+            statusCode,
 
-    });
+        });
 })
